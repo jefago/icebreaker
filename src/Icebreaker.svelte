3effb3c06046603891e466b4793ca0132eb3e6e6
Jens-Fabian Goetzmann <mail@jefago.com>
2022-01-28 13:48:29 +0100

    Attribution of photographer

--- src/Icebreaker.svelte
+++ src/Icebreaker.svelte
@@ -5,10 +5,17 @@ import LoadingSpinner from "./LoadingSpinner.svelte";
 
   export let keyword : string;
   export let question : string;
-  
+
+  type AuthorInfo = {
+    name : string;
+    profileUrl : string;
+  }
+
+
   const shown = writable<boolean>(false);
   const background = writable<string>("");
   const displayedQuestion = writable<string>("");
+  const authorInfo = writable<AuthorInfo|null>(null);
 
   const apiKey = process.env.UNSPLASH_API_KEY;
 
@@ -16,10 +23,11 @@ import LoadingSpinner from "./LoadingSpinner.svelte";
     shown.set(false);
 
 
-    const finish = (newBackground : string) => {
+    const finish = (newBackground : string, author?: AuthorInfo) => {
       background.set(newBackground);
       shown.set(true);
       displayedQuestion.set(question);
+      authorInfo.set(author ?? null);
     }
 
     fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}&query=${keyword}`)
@@ -28,11 +36,11 @@ import LoadingSpinner from "./LoadingSpinner.svelte";
       return res.json();
     })
     .then(json => {
-      console.log(JSON.stringify(json));
+      console.log(JSON.stringify(json, null, 2));
       let img = new Image();
       img.src = json.urls.regular;
       img.onload = () => {
-        finish(`url(${img.src})`);
+        finish(`url(${img.src})`, {name: json.user.name, profileUrl: json.user.links.html});
       }
       img.onerror = () => {
         finish('black');
@@ -46,6 +54,13 @@ import LoadingSpinner from "./LoadingSpinner.svelte";
 
 <div class="icebreaker {$shown ? "shown" : "hidden"}" style="background:{$background}">
   <h1>{$displayedQuestion}</h1>
+  {#if $authorInfo }
+    <div class="authorInfo">
+      Photo by
+      <a href="{$authorInfo.profileUrl}?utm_source=Icebreaker&utm_medium=referral" rel="noopener noreferrer">{$authorInfo.name}</a>
+      on
+      <a href="https://unsplash.com?utm_source=Icebreaker&utm_medium=referral" rel="noopener noreferrer">Unsplash</a></div>
+  {/if}
 </div>
 {#if !$shown}
   <LoadingSpinner />
@@ -64,6 +79,21 @@ import LoadingSpinner from "./LoadingSpinner.svelte";
     align-items: center;
     justify-content: center;
   }
+
+  div.authorInfo {
+    position:absolute;
+    right:0px;
+    bottom:0px;
+    background-color: black;
+    padding:6px;
+    color:white;
+    font-size:12px;
+    font-family:Arial, Helvetica, sans-serif;
+  }
+
+  div.authorInfo a, div.authorInfo a:link, div.authorInfo a:visited {
+    color:white;
+  } 
 
   h1 {
     font-size:4em;
